e07d668bf11079bc89a5340be091f622642f1a0f
Gabriel Costa <gubsocosta@gmail.com>
2021-01-04 19:49:04 -0300

    creating find by id route

--- api/controllers/PeopleController.js
+++ api/controllers/PeopleController.js
@@ -14,6 +14,25 @@ class PeopleController {
                 .json(error.message);
         }
     }
+
+    static async findById(req, res) {
+        const { id } = req.params;
+        try {
+            const person = await database
+                .People
+                .findOne({ 
+                    where: { id: Number(id) }
+                });
+            
+            return res
+                .status(200)
+                .json(person);
+        } catch (error) {
+            return res
+                .status(500)
+                .json(error.message);
+        }
+    }
 }
 
 module.exports = PeopleController;
